7e4f1729175d223d9eb3cf146a094469004a02ca
無奈枫葉 <742317550@qq.com>
2020-05-13 17:08:49 +0800

    表单的双向数据绑定和表单的验证规则rules和prop

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,27 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   extends: [
     'plugin:vue/essential',
     '@vue/standard'
   ],
+
   parserOptions: {
     parser: 'babel-eslint'
   },
+
   rules: {
-    'semi': 0,
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    semi: 0,
+    'no-console': 'off',
+    'no-debugger': 'off'
+  },
+
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ]
 }
--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import router from './router'
 import './plugins/element.js'
 // 引入全局 css
 import './assets/css/global.css'
-
+// 导入字体图标
+import './assets/fonts/iconfont.css';
 Vue.config.productionTip = false
 
 new Vue({
